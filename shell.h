52e96e94576dd1ff9d2dac3ae7f2b7900eae03c2
CanonicCake <n.a.ellis99@gmail.com>
2023-08-01 09:44:30 -0700

    libraries and prototypes

--- shell.h
+++ shell.h
@@ -1,11 +1,24 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 typedef struct shell{
 	int
 	char
 } shell_struct
 
-unsigned long int simple_shell(shell_struct *power);
+unsigned long int simple_shell(shell_struct *parameter);
 
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
 #include "shell.h"
 
 /**
  * simple_shell
- *
+ * @parameter:
+ * 
+ * Return: loop and input, error otherwise
+ */
+
+unsigned long int simple_shell(shell_struct *parameter)
+{
+	
+}
