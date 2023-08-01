aad58b56c42c59aafef2c3db823f852345a44c97
CanonicCake <n.a.ellis99@gmail.com>
2023-08-01 07:34:35 -0700

    a beautiful code to pass betty

---
+++ simple_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.c>
+#include <stdlib.c>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry into shell
+ * 
+ * Return: 0 always (success)
+ */
+
+int main(void)
+{
+	printf("Hello world!");
+	return (0);
+}
