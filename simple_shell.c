0ee086c9d33ee69dfc9710f5c2a38b864de3dc45
Nick <nicholasyoung548@yahoo.com>
2023-08-03 16:50:24 -0700

    added ctrl+d fix

--- simple_shell.c
+++ simple_shell.c
@@ -8,17 +8,20 @@
  */
 int shell (const char* parameters, unsigned long int command)
 unsigned long int simple_shell(shell_struct *parameter)
-
+{
 	size_t simple_shell(const char *parameters, unsigned long int command)
 	int buffer /*find buffer value*/, counter = 0;
 	int bytes_read = 0;
 
-{
 	while(1)
 	{
-		counters++
-			isatty(STDIN) = 1 ?write(1, "$ ",2);0;
+		counters++;
+		isatty(STDIN) = 1 ? write(1, "$ ",2) : 0;
 		byte_read = getline(&buffer, &parameters, stdin);
-	if()
+		if(command == 1)
+		{
+			free(buffer);
+			free(parameters);
 		}
+	}
 }
