8bbe97e7987ea8f50fa0fbd952a9f939a22d1290
CanonicCake <n.a.ellis99@gmail.com>
2023-08-06 18:11:54 -0700

    one big loop, changing simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,27 +1,49 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * simple_shell
- * @parameter:
+ * main - one entry to interact with shell
  * 
- * Return: loop and input, error otherwise
+ * Return: exit status on failure, and PATH on success
  */
-int shell (const char* parameters, unsigned long int command)
-unsigned long int simple_shell(shell_struct *parameter)
-{
-	size_t simple_shell(const char *parameters, unsigned long int command)
-	int buffer /*find buffer value*/, counter = 0;
-	int bytes_read = 0;
 
-	while(1)
+int main (void)
+{
+	size_t pointer = 0; /*pointer allows stdin and STDIN_FILENO to getline a string*/
+	int bytes_read, counter = 0, parameters = 0;
+	/* bytes_read and buffer are simular in the way they are connected by memory*/
+	/* parameters are the structure in which everything falls under*/
+	char *buffer = NULL, *token = NULL;
+	/* token is an array of tokenized strings*/
+
+	while (1)
 	{
-		counters++;
-		isatty(STDIN) = 1 ? write(1, "$ ",2) : 0;
-		byte_read = getline(&buffer, &parameters, stdin);
-		if(command == 1)
+		counter++;
+		isatty(STDIN_FILENO) == 1 ? write (1, "usr$ ", 2) : 0;
+		/* ?: is a conditional on if true*/
+		if (bytes_read == -1) /* exit status and cntl-d*/
 		{
 			free(buffer);
 			free(parameters);
 		}
+
+		token = tokenize(buffer);
+		if (!token[0]) /* checks for space*/
+		{
+			free_grid(token);
+			continue;
+		}
+
+		if (_strcmp(token[0], "env") == 0 && !token[1]) /*compares token and env*/
+		/*env is one of our builtins*/
+		{
+			print_env();
+			continue;
+		}
+		/*exit*/
+		if (_strcmp(token[0], "exit") == 0 && !token[1])
+		{
+			
+		}
 	}
+
 }
