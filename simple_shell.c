7ed6dc67a77154b496c7cc46521e0e49170eb16a
Nick <nicholasyoung548@yahoo.com>
2023-08-07 17:17:35 -0700

    Version Ahhhhhh

--- simple_shell.c
+++ simple_shell.c
@@ -9,22 +9,21 @@
 int main (void)
 {
 	size_t pointer = 0; /*pointer allows stdin and STDIN_FILENO to getline a string*/
-	int bytes_read, counter = 0, *parameters = 0;
+	int bytes_read, counter = 0, parameters = 0;
 	/* bytes_read and buffer are simular in the way they are connected by memory*/
 	/* parameters are the structure in which everything falls under*/
-	char *buffer = NULL, *token = NULL;
+	char *buffer = NULL, **token = NULL;
 	/* token is an array of tokenized strings*/
 
 	while (1)
 	{
 		counter++;
-		isatty(STDIN_FILENO) == 1 ? write (1, "usr$ ", 2) : 0;
+		isatty(STDIN_FILENO) == 1 ? write (1, "usr$ ", 5) : 0;
 		/* ?: is a conditional on if true*/
 		bytes_read = getline(&buffer, &pointer, stdin);
 		if (bytes_read == -1) /* exit status and cntl-d*/
 		{
 			free(buffer);
-			free(parameters);
 		}
 
 		token = malloc(**tokenize(buffer));
@@ -34,19 +33,19 @@ int main (void)
 			continue;
 		}
 
-		if (strcmp(token, "env") == 0 && !token[1]) /*compares token and env*/
+		if (strcmp(token[0], "env") == 0 && !token[1]) /*compares token and env*/
 		/*env is one of our builtins*/
 		{
 			print_env();
 			continue;
 		}
 		/*exit*/
-		if (strcmp(token, "exit") == 0 && !token[1])
+		if (strcmp(token[0], "exit") == 0 && !token[1])
 		{
-			shell_exit (token, buffer, *parameters);
+			shell_exit (*token, buffer, parameters);
 		}
 		/* parameters will search for a valid PATH*/
-		parameters = execve(token, buffer, *counter, parameters);
+		parameters = execute(token, buffer, counter, parameters);
 	}
 
 }
